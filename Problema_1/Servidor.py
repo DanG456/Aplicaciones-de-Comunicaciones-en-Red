10dcc027524e8b7b2450d204a3ceb531ed64ba1a
DanG456 <80288370+DanG456@users.noreply.github.com>
2021-12-19 22:26:09 -0600

    Update Servidor.py

--- Problema_1/Servidor.py
+++ Problema_1/Servidor.py
@@ -4,7 +4,30 @@ import time
 import sys
 import threading
 import pickle
-from Personaje import *
+import json
+from portaudio import *
+import pyaudio
+
+class Personaje():
+    def __init__(self, nombre="", cabello="", ojos="", piel="", accesorio="", genero=""):
+        self.CABELLO = 0
+        self.OJOS = 1
+        self.PIEL = 2
+        self.ACCESORIO = 3
+        self.GENERO = 4
+        self.nombre = nombre
+        self.caracteristicas = []
+        self.caracteristicas.append(cabello)
+        self.caracteristicas.append(ojos)
+        self.caracteristicas.append(piel)
+        self.caracteristicas.append(accesorio)
+        self.caracteristicas.append(genero)
+
+    def DescripcionPersonaje(self):
+        print("\t" + self.nombre + " [" + "Cabello: " + self.caracteristicas[self.CABELLO] + ", Ojos: " +
+              self.caracteristicas[self.OJOS] + ", Piel: " + self.caracteristicas[self.PIEL] + ", Accesorio: " +
+              self.caracteristicas[self.ACCESORIO] + ",Genero: " + self.caracteristicas[self.GENERO] + "]\n")
+        return
 
 BUFFER_SIZE = 1024
 JUEGO_TERMINADO = True
@@ -18,25 +41,30 @@ personaje = Personaje()
 listaconexiones = []
 identificadores = []
 tiros_anteriores = []
+host = "127.0.0.1"
+port = 65432
+
 
 def InicializarJuego():
     global JUEGO_TERMINADO
     JUEGO_TERMINADO = False
     global personaje
-    personaje = random.choice( personajes )
+    personaje = random.choice(personajes)
+
 
 def CargarPersonajes():
-    personajes.append(Personaje("Daniel", "Negro", "Cafes", "Morena", "Lentes", "Hombre"))
-    personajes.append(Personaje("Norman", "Negro", "Cafes", "Morena", "Nada", "Hombre"))
-    personajes.append(Personaje("Jassiel", "Cafe", "Cafes", "Blanca", "Lentes", "Hombre"))
-    personajes.append(Personaje("Alejandra", "Negro", "Cafes", "Morena", "Lentes", "Mujer"))
-    personajes.append(Personaje("Carlos", "Blanco", "Cafes", "Blanca", "Sombrero", "Hombre"))
-    personajes.append(Personaje("Cameron", "Rubio", "Azules", "Blanca", "Corbata", "Hombre"))
-    personajes.append(Personaje("Lizbeth", "Rojo", "Cafes", "Blanca", "Nada", "Mujer"))
-    personajes.append(Personaje("Diego", "Rubio", "Verdes", "Morena", "Sombrero", "Hombre"))
-    personajes.append(Personaje("Giselle", "Cafe", "Grises", "Morena", "Nada", "Mujer"))
-    personajes.append(Personaje("Erick", "Blanco", "Cafes", "Morena", "Sombrero", "Hombre"))
-    personajes.append(Personaje("Amanda", "Rojo", "Azules", "Blanca", "Sombrero", "Mujer"))
+    perso = "Personajes.json"
+    with open(perso,"r") as crearPerso:
+        personajeData = json.load(crearPerso)
+        nombre = personajeData["nombre"]
+        cabello = personajeData["cabello"]
+        ojos = personajeData["ojos"]
+        piel = personajeData["piel"]
+        accesorio = personajeData["accesorio"]
+        genero = personajeData["genero"]
+        len_personajes = len(nombre)
+        for i in range(len_personajes):
+            personajes.append(Personaje(nombre[i],cabello[i],ojos[i],piel[i],accesorio[i],genero[i]))
 
 
 def ServirPorSiempre(socketTcp, numeroConexiones):
@@ -45,62 +73,67 @@ def ServirPorSiempre(socketTcp, numeroConexiones):
         condicionTurnoActivo = threading.Condition()
         numero_cliente = 1
         while True:
-            if( JUEGO_TERMINADO ):
-                InicializarJuego() #Selección aleatoriamente de un nuevo personaje
+            if (JUEGO_TERMINADO):
+                InicializarJuego()  # Selección aleatoriamente de un nuevo personaje
 
             client_conn, client_addr = socketTcp.accept()
             print("Conectado a", client_addr)
             listaconexiones.append(client_conn)
-            
-            #Ejecuta la función de recibir pregunta para el cliente conectado
-            thread_read = threading.Thread(target=RecibirPregunta, args=[client_conn, client_addr, condicionEsperarJugadores, condicionTurnoActivo, numero_cliente, ])
+
+            # Ejecuta la función de recibir pregunta para el cliente conectado
+            thread_read = threading.Thread(target=RecibirPregunta,
+                                           args=[client_conn, client_addr, condicionEsperarJugadores,
+                                                 condicionTurnoActivo, numero_cliente, ])
             thread_read.start()
 
             with condicionEsperarJugadores:
-                if( int(numeroConexiones) == len(listaconexiones) ):
-                    print( "Se han conectado todos los jugadores" )                    
-                    thread_read_tiros = threading.Thread(target=GestionarTiros, args=[condicionTurnoActivo, condicionEsperarJugadores, ])
+                if (int(numeroConexiones) == len(listaconexiones)):
+                    print("Se han conectado todos los jugadores")
+                    thread_read_tiros = threading.Thread(target=GestionarTiros,
+                                                         args=[condicionTurnoActivo, condicionEsperarJugadores, ])
                     thread_read_tiros.start()
                     numero_cliente = 0
                 else:
-                    print( "En espera de " + str(int(numeroConexiones) - len(listaconexiones)) + " conexiones" )
-                condicionEsperarJugadores.notifyAll()            
-            
+                    print("En espera de " + str(int(numeroConexiones) - len(listaconexiones)) + " conexiones")
+                condicionEsperarJugadores.notifyAll()
+
             numero_cliente = numero_cliente + 1
             gestion_conexiones()
     except Exception as e:
         print(e)
 
-def GestionarTiros(condicionTurnoActivo, condicionEsperarJugadores):    
+
+def GestionarTiros(condicionTurnoActivo, condicionEsperarJugadores):
     global JUEGO_TERMINADO
     global ID_TURNO
-    
+
     turnos = 0
-    
-    while(not JUEGO_TERMINADO):
+
+    while (not JUEGO_TERMINADO):
         time.sleep(0.5)
-        #Determina que jugador puede tirar
+        # Determina que jugador puede tirar
         with condicionTurnoActivo:
             ID_TURNO = identificadores[turnos % len(identificadores)]
-            print( "Turno del jugador: " + str(ID_TURNO) )
-            #Notifica que ya se ha determinado al jugador que tiene el turno
+            print("Turno del jugador: " + str(ID_TURNO))
+            # Notifica que ya se ha determinado al jugador que tiene el turno
             condicionTurnoActivo.notifyAll()
 
-        #Espera a que el jugador notifique que termino su turno
+        # Espera a que el jugador notifique que termino su turno
         with condicionEsperarJugadores:
             condicionEsperarJugadores.wait()
-        turnos = turnos + 1 
-    
+        turnos = turnos + 1
+
     with condicionTurnoActivo:
         condicionTurnoActivo.notifyAll()
 
     time.sleep(0.5)
-    #Manda el resultado final a los clientes
+    # Manda el resultado final a los clientes
     EnviarTirosAClientes(True, ID_TURNO)
     personaje = Personaje()
     listaconexiones.clear()
     tiros_anteriores.clear()
     print("hilos activos:", threading.active_count())
+
 
 def gestion_conexiones():
     for conn in listaconexiones:
@@ -109,16 +142,17 @@ def gestion_conexiones():
     print("hilos activos:", threading.active_count())
     print("conexiones: ", len(listaconexiones))
 
-def EnviarTirosAClientes(JUEGO_TERMINADO = False, identificador = ""):
+
+def EnviarTirosAClientes(JUEGO_TERMINADO=False, identificador=""):
     i = 0
     for conn in listaconexiones:
         datoEnviar = []
-        datoEnviar.append( identificadores[i] == ID_TURNO )
-        datoEnviar.append( identificadores[i] )
-        datoEnviar.append( tiros_anteriores )
-        datoEnviar.append( JUEGO_TERMINADO )
-        if( JUEGO_TERMINADO):
-            if ( identificador == identificadores[i] ):
+        datoEnviar.append(identificadores[i] == ID_TURNO)
+        datoEnviar.append(identificadores[i])
+        datoEnviar.append(tiros_anteriores)
+        datoEnviar.append(JUEGO_TERMINADO)
+        if (JUEGO_TERMINADO):
+            if (identificador == identificadores[i]):
                 resultado = "Has ganado"
             else:
                 resultado = "Has perdido"
@@ -126,79 +160,78 @@ def EnviarTirosAClientes(JUEGO_TERMINADO = False, identificador = ""):
         else:
             resultado = ""
             nombre = ""
-        datoEnviar.append( resultado )
-        datoEnviar.append( nombre )
+        datoEnviar.append(resultado)
+        datoEnviar.append(nombre)
         i += 1
-        conn.sendall( pickle.dumps(datoEnviar) )
-        
-        if( JUEGO_TERMINADO ):
-            print( "Cerrando conexión " + str(i) )
-            conn.close()   
+        conn.sendall(pickle.dumps(datoEnviar))
+
+        if (JUEGO_TERMINADO):
+            print("Cerrando conexión " + str(i))
+            conn.close()
+
 
 def CompararCaracteristica(caracteristica, valor):
-    if ( caracteristica == "nombre" ):
-        if(personaje.nombre.lower() == valor):
+    if (caracteristica == "nombre"):
+        if (personaje.nombre.lower() == valor):
             global JUEGO_TERMINADO
             JUEGO_TERMINADO = True
         return personaje.nombre.lower() == valor
-    elif ( caracteristica == "cabello" ):
+    elif (caracteristica == "cabello"):
         return personaje.caracteristicas[CABELLO].lower() == valor
-    elif ( caracteristica == "ojos" ):
+    elif (caracteristica == "ojos"):
         return personaje.caracteristicas[OJOS].lower() == valor
-    elif ( caracteristica == "piel" ):
+    elif (caracteristica == "piel"):
         return personaje.caracteristicas[PIEL].lower() == valor
-    elif ( caracteristica == "accesorio" ):
+    elif (caracteristica == "accesorio"):
         return personaje.caracteristicas[ACCESORIO].lower() == valor
-    elif ( caracteristica == "genero" ):
+    elif (caracteristica == "genero"):
         return personaje.caracteristicas[GENERO].lower() == valor
 
+
 def RecibirPregunta(conn, addr, condicionEsperarJugadores, condicionTurnoActivo, identificador):
-    #Esperando el inicio del juego
+    # Esperando el inicio del juego
     while True:
         with condicionEsperarJugadores:
-            #Esperando notificación de un nuevo cliente conectado
+            # Esperando notificación de un nuevo cliente conectado
             condicionEsperarJugadores.wait()
-            conn.sendall( str(conexiones - len(listaconexiones)).encode() )
-            if( conexiones == len(listaconexiones) ):
+            conn.sendall(str(conexiones - len(listaconexiones)).encode())
+            if (conexiones == len(listaconexiones)):
                 print("Enviando inicio de juego")
-                break 
-    
+                break
+
+
     tiro_anterior = ""
-    while not JUEGO_TERMINADO:        
+    while not JUEGO_TERMINADO:
         with condicionTurnoActivo:
-            #Esperando a que gestión de tiros determine el siguiente turno
-            condicionTurnoActivo.wait()            
-            if( ID_TURNO == identificador and not JUEGO_TERMINADO):                
-                #Espera el tiro del cliente
+            # Esperando a que gestión de tiros determine el siguiente turno
+            condicionTurnoActivo.wait()
+            if (ID_TURNO == identificador and not JUEGO_TERMINADO):
+                # Espera el tiro del cliente
                 with condicionEsperarJugadores:
                     EnviarTirosAClientes(JUEGO_TERMINADO, identificador)
-                    print( "Esperando tiro del cliente: ", addr )
-                    tiroCliente = pickle.loads( conn.recv(BUFFER_SIZE) )
-                    comparacion = CompararCaracteristica( tiroCliente[0].lower(), tiroCliente[1].lower() )
-                    if( comparacion ):
-                        conn.sendall( str("Correcto, el personaje tiene : " + tiroCliente[0] + " " + tiroCliente[1] ).encode() )
+                    print("Esperando tiro del cliente: ", addr)
+                    tiroCliente = pickle.loads(conn.recv(BUFFER_SIZE))
+                    comparacion = CompararCaracteristica(tiroCliente[0].lower(), tiroCliente[1].lower())
+                    if (comparacion):
+                        conn.sendall(
+                            str("Correcto, el personaje tiene : " + tiroCliente[0] + " " + tiroCliente[1]).encode())
                         tiroCliente.append("Si")
                     else:
-                        conn.sendall( str("El personaje no tiene : " + tiroCliente[0] + " " + tiroCliente[1] ).encode() )
+                        conn.sendall(str("El personaje no tiene : " + tiroCliente[0] + " " + tiroCliente[1]).encode())
                         tiroCliente.append("No")
-                    
-                    tiros_anteriores.append(tiroCliente)
-                    condicionEsperarJugadores.notify() #Notifica al gestor de tiros que el cliente ha tirado
-            else:
-                print ("No es turno del jugador: " + str(identificador) )
 
-    print("SALIENDO jugador " + str(identificador)  +  " ... ")
+                    tiros_anteriores.append(tiroCliente)
+                    condicionEsperarJugadores.notify()  # Notifica al gestor de tiros que el cliente ha tirado
+            else:
+                print("No es turno del jugador: " + str(identificador))
+
+    print("SALIENDO jugador " + str(identificador) + " ... ")
+
 
 def MostrarPersonajes():
     print("Personajes:")
     for personaje in personajes:
         personaje.DescripcionPersonaje()
-
-host, port, numConn = sys.argv[1:4]
-
-if len(sys.argv) != 4:
-    print("usage:", sys.argv[0], "<host> <port> <numero jugadores>")
-    sys.exit(1)
 
 serveraddr = (host, int(port))
 conexiones = 0
@@ -206,12 +239,13 @@ conexiones = 0
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPServerSocket:
     TCPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     TCPServerSocket.bind(serveraddr)
+    numConn = input("Introduce el numero de jugadores: ")
     TCPServerSocket.listen(int(numConn))
     conexiones = int(numConn)
-    print( "El servidor TCP está disponible y en espera de solicitudes" )
+    print("El servidor TCP está disponible y en espera de solicitudes")
     CargarPersonajes()
-
+    MostrarPersonajes()
     for i in range(1, int(numConn) + 1):
-        identificadores.append( i )
-    
+        identificadores.append(i)
+
     ServirPorSiempre(TCPServerSocket, int(numConn))
